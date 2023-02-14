e6c3354bdc297248230186fec1c9e9ededdac02c
KatkuriRamesh <43947781+KatkuriRamesh@users.noreply.github.com>
2023-02-14 11:03:44 +0530

    Update ParentApplication.java

--- parent/src/main/java/com/ibs/parent/ParentApplication.java
+++ parent/src/main/java/com/ibs/parent/ParentApplication.java
@@ -1,10 +1,6 @@
 package com.ibs.parent;
-
-import com.ibs.utility.converters.DateTimeConverters;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class ParentApplication {
@@ -13,7 +9,6 @@ public class ParentApplication {
 		System.out.println("application started .....");
 		SpringApplication.run(ParentApplication.class, args);
 		System.out.println("application stopped .....");
-		DateTimeConverters converters = new DateTimeConverters();
-		System.out.println(converters.dateTimeToLocalTime(LocalDateTime.now()));
+	
 	}
 }
